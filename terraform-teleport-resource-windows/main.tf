594c42bbfee6f99e88c0ea241dd63a3635d4182b
Peter ONeill <peteroneilljr@gmail.com>
2024-10-30 13:09:27 -0600

    update node vars

--- terraform-teleport-resource-windows/main.tf
+++ terraform-teleport-resource-windows/main.tf
@@ -40,39 +40,7 @@ module "windows_instances" {
   tags = var.aws_tags
 
 }
-# ---------------------------------------------------------------------------- #
-# Create Teleport Agent to proxy windows machines
-# ---------------------------------------------------------------------------- #
-module "windows_teleport" {
-  source = "git::https://github.com/peteroneilljr/terraform-teleport-agent.git"
 
-  aws_vpc_id            = var.aws_vpc_id
-  aws_security_group_id = var.aws_security_group_id
-  aws_subnet_id         = var.aws_subnet_id
-
-  aws_key_pair = var.aws_key_name
-  aws_tags = var.aws_tags
-
-  teleport_agent_roles = ["Node", "WindowsDesktop"]
-
-  teleport_nodename = "windows-agent"
-
-  teleport_proxy_address = var.teleport_proxy_address
-  teleport_version       = var.teleport_version
-  teleport_ssh_labels = {
-    "type" = "agent"
-  }
-
-  teleport_windows_hosts = {
-    for index, host in var.windows_machines: 
-    host => {
-      "addr" = module.windows_instances["${host}"].private_ip
-      "labels" = {
-        "env"  = "${host}"
-      }
-    }
-  }
-}
 # ---------------------------------------------------------------------------- #
 # Windows AMI Lookup
 # ---------------------------------------------------------------------------- #
---
+++ terraform-teleport-resource-windows/teleport.tf
@@ -0,0 +1,34 @@
+# ---------------------------------------------------------------------------- #
+# Create Teleport Agent to proxy windows machines
+# ---------------------------------------------------------------------------- #
+module "windows_teleport" {
+  source = "git::https://github.com/peteroneilljr/terraform-teleport-agent.git"
+
+  aws_vpc_id            = var.aws_vpc_id
+  aws_security_group_id = var.aws_security_group_id
+  aws_subnet_id         = var.aws_subnet_id
+
+  aws_key_pair = var.aws_key_name
+  aws_tags = var.aws_tags
+
+  teleport_agent_roles = ["Node", "WindowsDesktop"]
+
+  teleport_node_name = "windows-agent"
+  teleport_node_labels = {
+    "type" = "agent"
+  }
+  
+  teleport_proxy_address = var.teleport_proxy_address
+  teleport_version       = var.teleport_version
+
+
+  teleport_windows_hosts = {
+    for index, host in var.windows_machines: 
+    host => {
+      "addr" = module.windows_instances["${host}"].private_ip
+      "labels" = {
+        "env"  = "${host}"
+      }
+    }
+  }
+}

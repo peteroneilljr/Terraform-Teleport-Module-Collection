bfd8c417f416853dbaa2d0ce7fc3d8d349b95bb2
Peter ONeill <peteroneilljr@gmail.com>
2024-10-30 14:21:49 -0600

    output teleport_windows_hosts

---
+++ terraform-teleport-resource-windows/outputs.tf
@@ -0,0 +1,13 @@
+output teleport_windows_hosts {
+  description = "description"
+  
+  value = {
+    for index, host in var.windows_machines: 
+    host => {
+      "addr" = module.windows_instances["${host}"].private_ip
+      "labels" = {
+        "env"  = "${host}"
+      }
+    }
+  }
+}
